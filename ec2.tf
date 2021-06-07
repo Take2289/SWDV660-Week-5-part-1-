6d34d7086fcc97ea23cedd2e4698418f5a346c2d
Ty Davidson <62067132+Take2289@users.noreply.github.com>
2021-06-06 20:59:12 -0400

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+access_key = "AKIAZLLZUDMRRNICKD6B"
+secret_key = "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"
+region = "us-east-1"
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_Jenkins2"
+  description = "Allow Jenkins inbound traffic"
+  vpc_id      = "vpc-0ce130e0294cace0d"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+   ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+     ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+# Resource Configuration for AWS
+resource "aws_instance" "myjenkinsserver" {
+  ami = "ami-cfe4b2b0"
+  instance_type = "t2.micro"
+  key_name = "EffectiveDevOpsAWS"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.name}"]
+  tags = {
+  Name = "Jenkins_Class"
+  }
+}
